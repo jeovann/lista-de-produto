d65e96b5c333c7d2004bb29431e7e74002a23d3b
jeovan <jeovann@hotmail.com>
2018-04-10 19:34:13 -0400

    19:34

---
+++ src/modelo/Produtos.java
@@ -0,0 +1,34 @@
+package modelo;
+
+public class Produtos {
+	private String nome;
+	private Float valor;
+	private int quantidade;
+	private Float resultado;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Float getValor() {
+		return valor;
+	}
+	public void setValor(Float valor) {
+		this.valor = valor;
+	}
+	public int getQuantidade() {
+		return quantidade;
+	}
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+	public Float getResultado() {
+		return resultado;
+	}
+	public void setResultado(Float resultado) {
+		this.resultado = resultado;
+	}
+	
+	}
+	
